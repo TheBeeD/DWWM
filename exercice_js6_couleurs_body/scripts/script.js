914259be2058c8fd52f04ce58a8730d5c901d3de
TheBeeD <olivb3d@gmail.com>
2022-04-12 22:37:01 +0200

    Renommage_dossiers_et_essai_infructueu_couleurs_js

---
+++ exercice_js6_couleurs_body/scripts/script.js
@@ -0,0 +1,15 @@
+
+const button = document.querySelector('#clack'); 
+
+button.addEventListener('click', function() {
+ 
+  document.body.style.background 
+ 
+})
+if(document.body.style.background = 
+  "black") {
+  button.addEventListener('click'), function() {
+    button.addEventListener('click', event => 
+    button.innerHTML = `Nombre de clics : ${document.body.style.background = Option( "red","orange","pink","indigo","violet","indigo")
+})};
+

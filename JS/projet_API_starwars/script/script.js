cceaa28e669fc3b955dffe8cc040b5642ce1ca4e
TheBeeD <olivb3d@gmail.com>
2022-05-05 07:52:38 +0200

    modifs_fonctions_sw

--- JS/projet_API_starwars/script/script.js
+++ JS/projet_API_starwars/script/script.js
@@ -1,8 +1,11 @@
 let planeteSection = document.getElementsByClassName("wrapper");
-let planets = Object.planets.document.createElement("div,.card");
+let planets;
+let nomPersonnages;
+let listFilms;
+
 
 var callBackGetSuccess = function (data) {
-console.log(data);
+    console.log(data);
 }
 //#region "Function chargeJson"
 function chargeJson() {
@@ -22,24 +25,24 @@ function chargeJson() {
 chargeJson();
 //#endregion "Function chargeJson"
 //#region "Function fetchAPI multiple"
-async function fetchAPI (url){
-    try{
-   
-    await fetch(url,{
-        mode: 'cors'
-    }) 
-        .then( async function(response) {   
-        
-            return   await response.json(); 
+async function fetchAPI(url) {
+    try {
+
+        await fetch(url, {
+            mode: 'cors'
         })
-        .then(function(json) {
-            let result;
-            result=json;
-         
-      });
+            .then(async function (response) {
+
+                return await response.json();
+            })
+            .then(function (json) {
+                let result;
+                result = json;
+
+            });
     } catch (error) {
         console.log("Error", error)
-      }
+    }
 }
 //#endregion "Function fetchAPI multiple"
 
@@ -48,20 +51,20 @@ async function fetchAPI (url){
 function buttonClickGET() {
     var url = "https://swapi.dev/api/films"
 
-    $.get( url, callBackGetSuccess ).done(function () {
+    $.get(url, callBackGetSuccess).done(function () {
 
     })
-    .fail(function() {
-        alert ( "erreur" );
-    })
-    .always(function() {
-    });
+        .fail(function () {
+            alert("erreur");
+        })
+        .always(function () {
+        });
 
 
 }
-
+planets.document.createElement("div,.card");
 let planetName = document.createElement("h2");
-  planetName.innerHTML = planets.planeteSection;
-  planetName.setAttribute("id", "nomPlanete");
-  planetName.append(planets);
-  console.log(planetName);  
+planetName.innerHTML = planets.planeteSection;
+planetName.setAttribute("id", "nomPlanete");
+planetName.append(planets);
+console.log(planetName);  

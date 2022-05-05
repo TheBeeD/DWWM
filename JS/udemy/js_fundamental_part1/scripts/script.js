1f6edc26b550e651b6a32a20097b1d425e58e1ee
27010-34-01 <27010-34-01@exchange.ad.afpanet>
2022-05-05 09:29:12 +0200

    udemy1

---
+++ JS/udemy/js_fundamental_part1/scripts/script.js
@@ -0,0 +1,13 @@
+let country;
+let continent;
+let population;
+let myCountry = "Evreux";
+let myContinent = "Europe";
+let myPopulation = "13";
+
+country = myCountry;
+continent = myContinent;
+population = myPopulation;
+
+console.log(country, continent, population);
+let isIsland = true;

565ccafd282c5310842c286fd3be85aaef4e059d
27010-34-01 <27010-34-01@exchange.ad.afpanet>
2022-05-09 11:40:35 +0200

    exo_function_imc

--- JS/UDEMY/exo 29 1 fonctions avec parametres/scripts/index.js
+++ JS/UDEMY/exo 29 1 fonctions avec parametres/scripts/index.js
@@ -1,8 +1,38 @@
 let preparation = 10, cuisson = 15;
 
-function cuisiner(nombreDeGateaux, minutesDeCuisson, minutesDePreparation) {
+function cuisiner(nombreDeGateaux, minutesDePreparation = 10,  minutesDeCuisson = 15) {
+
     let resultat = nombreDeGateaux * (minutesDeCuisson + minutesDePreparation);
     
     return resultat;
 }
-console.log(cuisiner(5, preparation, cuisson));
+// console.log(cuisiner(5, preparation, cuisson));
+
+let tempsDePreparationChocolat = cuisiner(4);
+let tempsDePreparationFraisier = cuisiner(1, 20);
+console.log(tempsDePreparationFraisier + tempsDePreparationChocolat);
+// function prevoirAge() {
+
+//     let age = prompt("Quel est votre age? "); // "30" + 1 = 301
+//     // age = parseInt(age); <-- parseInt revois un entier 
+//     //équivalent à la fonction Number qui suit:
+//     age = Number(age);// <-- est la fonction recommandée, ça fonctionne avec ou sans virgule
+//     //avec un nombre à virgule on utilsera parseFloat
+//     // pour faire l'inverse on a cet exemple: nombreEnString = nombre.toString();
+//     alert("Bientôt, vous aurez " + (age + 1) + " ans");
+
+// }
+
+// prevoirAge();
+
+
+// Fonctions anonymes:
+
+// let fonctionAnonyme = function() {
+//     console.log(`Je suis une fonction anonyme`);
+// }
+
+//fonctionAnonyme();
+
+// (function() { console.log('Je suis une fonction anonyme')})();
+    
---
+++ JS/UDEMY/exo 7_33_test_récap_fonctions_anonymes/scripts/index.js
@@ -0,0 +1,19 @@
+
+
+function abracadabra() {
+    let prenom = prompt("Saisir votre nom: ");
+    let nom = prompt("Saisir votre nom: ");
+    let age = prompt("Saisir age: ");
+
+
+
+alert("Sapristi ! On ne m'avait pas prévenu que c'était vous," 
++ prenom 
++ "! Euh... Je veux dire... Monsieur le grand magicien " 
++ nom 
++ " ! Cela fait déjà " 
++ age 
++ " ans que vous faites rayonner notre contrée !.");
+}
+
+abracadabra();
